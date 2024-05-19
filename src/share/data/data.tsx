d223554c76309b722835d9ec812b01c93d2f8e52
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-05-19 21:34:00 +0300

    feat: Added introductory slides to showcase project highlights

---
+++ src/share/data/data.tsx
@@ -0,0 +1,33 @@
+import Slide1Image from '../../assets/slide-intro1.png';
+import Slide2Image from '../../assets/slide-intro2.png';
+import Slide3Image from '../../assets/slide-intro3.png';
+import Slide4Image from '../../assets/slide-intro4.png';
+import Slide5Image from '../../assets/slide-intro5.png';
+
+export const introImages = [
+  {
+    id: "2gsbqy72",
+    img: Slide1Image,
+    desc: "Built an awesome project"
+  },
+  {
+    id: "2gsbqy73",
+    img: Slide2Image,
+    desc: "Create powerful native apps"
+  },
+  {
+    id: "2gsbqy74",
+    img: Slide3Image,
+    desc: "Leverage cutting-edge technologies"
+  },
+  {
+    id: "2gsbqy75",
+    img: Slide4Image,
+    desc: "Deliver seamless user experiences"
+  },
+  {
+    id: "2gsbqy76",
+    img: Slide5Image,
+    desc: "Transform ideas into reality"
+  }
+];
