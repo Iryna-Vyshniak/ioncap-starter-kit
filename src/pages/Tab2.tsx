46986ac63a83d0de509ce7dcfb33c42aa98809dd
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-05-22 22:53:07 +0300

    feat: Implemented Tab2 component

---
+++ src/pages/Tab2.tsx
@@ -0,0 +1,30 @@
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
+import React from 'react';
+
+const Tab2: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar color={'success'}>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>Tab 2</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding" scrollY={false}>
+        UI goes here...
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab2;
