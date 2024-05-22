396779f54f637a6219d78e5b988df860b937f496
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-05-22 22:00:16 +0300

    feat: Added menuPaths to data.ts

--- src/share/data/data.tsx
+++ src/share/data/data.tsx
@@ -1,3 +1,5 @@
+import { homeOutline, newspaperOutline } from 'ionicons/icons';
+
 import Slide1Image from '../../assets/slide-intro1.png';
 import Slide2Image from '../../assets/slide-intro2.png';
 import Slide3Image from '../../assets/slide-intro3.png';
@@ -6,28 +8,33 @@ import Slide5Image from '../../assets/slide-intro5.png';
 
 export const introImages = [
   {
-    id: "2gsbqy72",
+    id: '2gsbqy72',
     img: Slide1Image,
-    desc: "Built an awesome project"
+    desc: 'Built an awesome project',
   },
   {
-    id: "2gsbqy73",
+    id: '2gsbqy73',
     img: Slide2Image,
-    desc: "Create powerful native apps"
+    desc: 'Create powerful native apps',
   },
   {
-    id: "2gsbqy74",
+    id: '2gsbqy74',
     img: Slide3Image,
-    desc: "Leverage cutting-edge technologies"
+    desc: 'Leverage cutting-edge technologies',
   },
   {
-    id: "2gsbqy75",
+    id: '2gsbqy75',
     img: Slide4Image,
-    desc: "Deliver seamless user experiences"
+    desc: 'Deliver seamless user experiences',
   },
   {
-    id: "2gsbqy76",
+    id: '2gsbqy76',
     img: Slide5Image,
-    desc: "Transform ideas into reality"
-  }
+    desc: 'Transform ideas into reality',
+  },
+];
+
+export const menuPaths = [
+  {  id: '2g45fqy77', name: 'Home', url: '/app/list', icon: homeOutline },
+  {  id: '2g1erqy78', name: 'Settings', url: '/app/settings', icon: newspaperOutline },
 ];
