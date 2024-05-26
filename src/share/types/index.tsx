018ddd8783ebfd788a9febbf0532708394696aac
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-05-27 01:04:48 +0300

    feat: Added fields to type user and user list props

--- src/share/types/index.tsx
+++ src/share/types/index.tsx
@@ -1,6 +1,7 @@
-type User = {
+export type User = {
   picture: {
     thumbnail: string;
+    large: string;
   };
   name: {
     title: string;
@@ -9,8 +10,21 @@ type User = {
   };
   email: string;
   nat: string;
+  dob: {
+    age: number;
+    date: string;
+  };
+  gender: string;
+  location: {
+    city: string;
+    street: { number: number; name: string };
+    country: string;
+    state: string;
+  };
+  phone: string;
 };
 
 export type UserListProps = {
   users: User[];
+  presentingElement: HTMLElement | null;
 };
