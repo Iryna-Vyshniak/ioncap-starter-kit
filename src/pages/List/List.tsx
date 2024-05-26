ca89b18fb7ea360c84a8a2478f4f5c339624f030
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-05-26 22:19:07 +0300

    feat: Added user refresh functionality to List component
    - Implemented `handleRefresh` function to fetch new user data and update state on pull-to-refresh action.
    - Added `IonRefresher` component to trigger `handleRefresh` when the user pulls down to refresh.
    - Displayed loading skeleton (`UsersSkeleton`) while data is being fetched.
    - Added `IonButton` to clear the user list using `clearList` function.

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -7,15 +7,33 @@ import {
   IonIcon,
   IonMenuButton,
   IonPage,
+  IonRefresher,
+  IonRefresherContent,
+  IonSearchbar,
   IonTitle,
   IonToolbar,
+  RefresherEventDetail,
 } from '@ionic/react';
-import { trashBinOutline } from 'ionicons/icons';
+import { chevronDownCircleOutline, trashBinOutline } from 'ionicons/icons';
 
 import './List.css';
 
+import { useGetUsers } from '../../share/hooks/useGetUsers';
+import { useClearList } from '../../share/hooks/useClearList';
+
+import UsersList from '../../components/users/UsersList';
+import UsersSkeleton from '../../components/users/UsersSkeleton';
+
 const List: React.FC = () => {
   const page = useRef(null);
+  const { users, setUsers, fetchUsers, loading } = useGetUsers();
+  const { clearList } = useClearList();
+
+  const handleRefresh = async (event: CustomEvent<RefresherEventDetail>) => {
+    const data = await fetchUsers();
+    setUsers(data);
+    event.detail.complete();
+  };
 
   return (
     <IonPage ref={page}>
@@ -26,7 +44,7 @@ const List: React.FC = () => {
           </IonButtons>
           <IonTitle>List</IonTitle>
           <IonButtons slot="end">
-            <IonButton>
+            <IonButton onClick={() => clearList(setUsers)}>
               <IonIcon
                 slot="icon-only"
                 icon={trashBinOutline}
@@ -35,8 +53,23 @@ const List: React.FC = () => {
             </IonButton>
           </IonButtons>
         </IonToolbar>
+        <IonToolbar color={'success'}>
+          <IonSearchbar />
+        </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding">UI goes here...</IonContent>
+
+      <IonContent>
+        <IonRefresher slot="fixed" onIonRefresh={e => handleRefresh(e)}>
+          <IonRefresherContent
+            pullingIcon={chevronDownCircleOutline}
+            pullingText="Pull to refresh"
+            refreshingSpinner="circles"
+            refreshingText="Refreshing..."
+          />
+        </IonRefresher>
+        {loading && <UsersSkeleton />}
+        {!loading && <UsersList users={users} />}
+      </IonContent>
     </IonPage>
   );
 };
