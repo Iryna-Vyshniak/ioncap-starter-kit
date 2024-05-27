60c5255c3acd0944377ea54ef2bb2ff20c21d16b
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-05-28 01:06:11 +0300

    feat: Added animation to button when view did enter in Tab2 component

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,4 +1,5 @@
 import {
+  IonButton,
   IonButtons,
   IonContent,
   IonHeader,
@@ -6,14 +7,44 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
+  createAnimation,
+  useIonViewDidEnter,
 } from '@ionic/react';
-import React from 'react';
+import type { Animation } from '@ionic/react';
+import React, { useRef } from 'react';
 
 const Tab2: React.FC = () => {
+  const animationButtonRef = useRef<HTMLIonButtonElement | null>(null);
+  const animation = useRef<Animation | null>(null);
+
+  useIonViewDidEnter(() => {
+    animation.current! = createAnimation()
+      .addElement(animationButtonRef.current!)
+      .duration(1500)
+      .iterations(Infinity)
+      .direction('alternate')
+      .keyframes([
+        {
+          offset: 0,
+          '--background': '#8496a2',
+        },
+        {
+          offset: 0.5,
+          '--background': '#868f74',
+        },
+        {
+          offset: 1,
+          '--background': '#8496a2',
+        },
+      ]);
+
+    animation.current?.play();
+  });
+
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar color={'success'}>
+        <IonToolbar color="success">
           <IonButtons slot="start">
             <IonMenuButton />
           </IonButtons>
@@ -21,7 +52,11 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding" scrollY={false}>
-        UI goes here...
+        <div className="button-container">
+          <IonButton ref={animationButtonRef} className="ion-margin">
+            Join to my followers
+          </IonButton>
+        </div>
       </IonContent>
     </IonPage>
   );
