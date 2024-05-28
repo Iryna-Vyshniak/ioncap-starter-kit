f1bbc9c85c0d742ac5b2ec85763a5d566486b715
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-05-29 00:33:54 +0300

    feat: Add camera functionality to capture and display images
    - Added state to hold captured image using useState.
    - Implemented `takePicture` function using Capacitor Camera API to capture images with specified quality and settings.
    - Set captured image as base64 string to state and displayed it using an <img> tag.
    - Added an IonButton to trigger image capture and conditionally rendered the captured image below the button.

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,5 +1,6 @@
 import {
   IonButtons,
+  IonButton,
   IonContent,
   IonHeader,
   IonMenuButton,
@@ -7,9 +8,29 @@ import {
   IonTitle,
   IonToolbar,
 } from '@ionic/react';
-import React from 'react';
+import React, {useState} from 'react';
+import { Camera, CameraResultType } from '@capacitor/camera';
 
 const Tab1: React.FC = () => {
+  const [image, setImage] = useState<any>(null)
+
+  const takePicture = async () => {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Base64
+    });
+  
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    const img = `data:image/jpeg;base64,${image.base64String}`;
+  
+    // Can be set to the src of an image now
+    setImage(img);
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -20,7 +41,11 @@ const Tab1: React.FC = () => {
           <IonTitle>Take a picture</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding">UI goes here...</IonContent>
+      <IonContent className="ion-padding">
+        <IonButton expand='full' onClick={takePicture}>Take picture</IonButton>
+        {image && <img src={image} alt='picture'/>}
+      
+      </IonContent>
     </IonPage>
   );
 };
