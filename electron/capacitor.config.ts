c13fefaf739d541b2b04904b401bde02e5894cdd
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-06-02 14:11:41 +0300

    feat:  Initiated the electron platform, created the electron folder, updated setup configuration

---
+++ electron/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ioncap.starter.kit',
+  appName: 'ioncap-starter-kit',
+  webDir: 'dist'
+};
+
+export default config;
